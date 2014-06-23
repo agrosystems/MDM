2a0348f26aee3e1f83f8c87e1b64c02102491a04
ramulu <ramulu@dev096-GA-78LMT-S2>
2014-06-23 12:36:00 +0530

    Multi module git created

--- src/main/webapp/app1/services/LoginService.js.js
+++ src/main/webapp/app1/services/LoginService.js.js
@@ -3,16 +3,8 @@ angular.module("LoginService", []).
         factory('loginService', function($http) {
             return {
                 loginUser: function(data) {
-                    var user = data;
-                    return $http({
-                        method: 'get',
-                        url: '/mdm/userLogin/validateUser',
-                        params: {
-                            userName: user.userName,
-                            password: user.password,
-                        },
-                        async: false
-                    });
+                    return $http.post('/mdm/userLogin/validateUser', data)
                 }
             }
         });
+        
